f907e31cf6255c26c30ac3c81dcaab6c2fe2754d
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-18 00:44:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, elem) {
-  
+  return arr.push(elem);
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem) {
+  arr = arr.push(elem);
+  return arr;
 }
