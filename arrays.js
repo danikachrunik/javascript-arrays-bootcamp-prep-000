91a5e91ab3a72f2b0a267e1f2f00362da2eefb33
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-18 00:46:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,12 @@ function destructivelyAddElementToBeginningOfArray(arr, elem) {
   arr = arr.unshift(elem);
   return arr;
 }
+
+function addElementToEndOfArray(arr, elem) {
+  return arr.unshift(elem);
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem) {
+  arr = arr.push(elem);
+  return arr;
+}
